7e404ccdb5200731c4c4fdff4032b2672da4123a
miguelapb <150673252+miguelapb@users.noreply.github.com>
2023-11-13 12:21:16 +0100

    Add files via upload

---
+++ procesosporlote/procesosporlote.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Miguel Parada Bandera
+REM Fecha: 13/11/2023
+
+mkdir %USERPROFILE%\dam %USERPROFILE%\daw %USERPROFILE%\asir %USERPROFILE%\ceti %USERPROFILE%\smr
+
+mkdir %USERPROFILE%\dam\pract %USERPROFILE%\dam\docs %USERPROFILE%\dam\exam
+
+mkdir %USERPROFILE%\asir\pract %USERPROFILE%\asir\docs %USERPROFILE%\asir\exam
+
+mkdir %USERPROFILE%\daw\pract %USERPROFILE%\daw\docs %USERPROFILE%\daw\exam
+
+mkdir %USERPROFILE%\smr\pract %USERPROFILE%\smr\docs %USERPROFILE%\smr\exam
+
+mkdir %USERPROFILE%\ceti\pract %USERPROFILE%\ceti\docs %USERPROFILE%\ceti\exam
